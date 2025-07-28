e7f07a984fb993d929a94e58f00ad6ff81513604
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-07-28 21:35:08 +0530

    Add js-cookie dependency to package.json and package-lock.json

--- app/api/admin/quiz/[quizId]/evaluate/route.js
+++ app/api/admin/quiz/[quizId]/evaluate/route.js
@@ -1,4 +1,4 @@
-import clientPromise from '../../../../../../../lib/db';
+import clientPromise from '../../../../../../lib/db';
 import { calculateScore } from '../../../../../../../lib/scoring';
 import { z } from 'zod';
 
